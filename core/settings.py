e23ab6298e81d316e8107ec44f03c2638263379f
kay <minhthien.k@gmail.com>
2024-06-08 12:44:49 +0700

    update static

--- core/settings.py
+++ core/settings.py
@@ -177,7 +177,7 @@ PWA_APP_BACKGROUND_COLOR = '#ffffff'
 PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/'
 PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/portal/'
+PWA_APP_START_URL = '/portal/profile/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
